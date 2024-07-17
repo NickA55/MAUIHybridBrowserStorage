085f4fbbad2c4bfd33f51fd7fc4e8209de71bbdb
Nick <nalonge@gmail.com>
2024-07-17 10:14:00 -0400

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIHybridBrowserStorage;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIHybridBrowserStorage;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
